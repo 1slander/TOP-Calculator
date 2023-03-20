b7d471926a8c000e19329f4f7fb33325a5d42857
1slander <hectorgomendez@gmail.com>
2023-03-20 16:38:12 +0000

    Add functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TOP-Calculator
+
+<h2> Doing:</h2>
+
+- I have created functions for display.
+- Created functions for operations.
+- Showing numbers + operation display.
--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+//Variables
+const buttons = document.querySelectorAll(".btn");
+const screenValue = document.querySelector(".value");
+const screenOperation=document.querySelector(".operation");
+let value;
+let operator;
+const operation = [];
+
+
+//Events listeners
+
+buttons.forEach(button=>{
+ button.addEventListener('click',()=>{
+  //Operators
+  if(button.innerText==='+' || button.innerText==='-' || button.innerText==='x' || button.innerText==='÷'){
+   operation.push(value);
+   operator=button.innerText;
+   value=undefined;
+   showNumbers(value);
+   showOperation(operator);
+
+//Numbers
+
+  } else if(value===undefined){
+    value = `${button.innerText}`
+    //screenValue.innerText=value;
+    showNumbers(value);
+
+  }else{
+    value+=`${button.innerText}`
+    showNumbers(value);
+    };
+   
+ 
+  console.log(value)
+ })
+})
+
+
+
+
 // Math Operations
 
 function add(num1,num2){
@@ -30,11 +71,24 @@ function operate(operator,num1,num2){
     case '-':
     return subtract(num1,num2);
     break;
-    case '*':
+    case 'x':
     return multiply(num1,num2);
     break;
-    case '/':
+    case '÷':
     return divide(num1,num2);
     break;
  }
 }
+
+// display
+
+function showNumbers (value){
+ screenValue.innerText=value;
+ if(value===undefined)screenValue.innerText=" ";
+}
+
+function showOperation(operator){
+ screenOperation.innerText=operator;
+}
+
+
