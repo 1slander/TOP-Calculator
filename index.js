eb7898dd222e1ee7e88426aac633263f01e27c7a
1slander <hectorgomendez@gmail.com>
2023-03-20 14:46:38 +0000

    Add html Structure

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 // Math Operations
 
-function add(){
+function add(num1,num2){
+ return num1 + num2;
 
 }
 
-function subtract(){
+function subtract(num1,num2){
+ return num1-num2;
 
 }
 
-function multiply(){
+function multiply(num1,num2){
+ return num1*num2;
 
 }
 
-function divide(){
- 
+function divide(num1,num2){
+ return num1/num2;
+
+}
+
+//Operate
+
+function operate(operator,num1,num2){
+ switch(operator){
+  case '+':
+    return add(num1,num2);
+    break;
+    case '-':
+    return subtract(num1,num2);
+    break;
+    case '*':
+    return multiply(num1,num2);
+    break;
+    case '/':
+    return divide(num1,num2);
+    break;
+ }
 }
