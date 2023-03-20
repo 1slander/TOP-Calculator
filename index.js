adf58c9e1c6d27a7b6ee1b743eb0936936d9bc88
1slander <hectorgomendez@gmail.com>
2023-03-20 21:09:49 +0000

    Added operations

--- index.js
+++ index.js
@@ -1,94 +1,113 @@
 //Variables
-const buttons = document.querySelectorAll(".btn");
+const btnDigits = document.querySelectorAll(".btn.digits");
+const btnOperator = document.querySelectorAll(".btn.operators");
+const btnEqual = document.querySelector(".equal");
+
 const screenValue = document.querySelector(".value");
-const screenOperation=document.querySelector(".operation");
+const screenOperation = document.querySelector(".operation");
 let value;
+let num1;
+let num2;
+let total;
 let operator;
 const operation = [];
 
-
 //Events listeners
 
-buttons.forEach(button=>{
- button.addEventListener('click',()=>{
-  //Operators
-  if(button.innerText==='+' || button.innerText==='-' || button.innerText==='x' || button.innerText==='÷'){
-   operation.push(value);
-   operator=button.innerText;
-   value=undefined;
-   showNumbers(value);
-   showOperation(operator);
+btnDigits.forEach((button) => {
+  button.addEventListener("click", () => {
+    //Numbers
+    if (value === undefined) {
+      value = `${button.innerText}`;
+      //screenValue.innerText=value;
+      showNumbers(value);
+    } else {
+      value += `${button.innerText}`;
+      showNumbers(value);
+    }
+    if (!num1 && !num2) {
+      num1 = +value;
+      value = undefined;
+    } else if (num1 !== undefined && !num2) {
+      console.log(num1);
+      console.log(value);
+      num2 = +value;
+      value = undefined;
+    } else if (num1 !== undefined && num2 !== undefined) {
+      num1 = +total;
+      num2 = +value;
+      value = undefined;
+    }
+  });
+});
 
-//Numbers
+btnOperator.forEach((button) => {
+  button.addEventListener("click", () => {
+    //Operators
+    switch (button.innerText) {
+      case "+":
+      case "-":
+      case "x":
+      case "÷":
+        operator = button.innerText;
+        break;
+    }
 
-  } else if(value===undefined){
-    value = `${button.innerText}`
-    //screenValue.innerText=value;
     showNumbers(value);
+    showOperation(operator);
+  });
+});
 
-  }else{
-    value+=`${button.innerText}`
-    showNumbers(value);
-    };
-   
- 
-  console.log(value)
- })
-})
-
-
-
+btnEqual.addEventListener("click", () => {
+  total = operate(num1, operator, num2);
+  showNumbers(total);
+});
 
 // Math Operations
 
-function add(num1,num2){
- return num1 + num2;
-
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(num1,num2){
- return num1-num2;
-
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(num1,num2){
- return num1*num2;
-
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(num1,num2){
- return num1/num2;
-
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
 //Operate
 
-function operate(operator,num1,num2){
- switch(operator){
-  case '+':
-    return add(num1,num2);
-    break;
-    case '-':
-    return subtract(num1,num2);
-    break;
-    case 'x':
-    return multiply(num1,num2);
-    break;
-    case '÷':
-    return divide(num1,num2);
-    break;
- }
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      //console.log("suma");
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "x":
+      return multiply(num1, num2);
+      break;
+    case "÷":
+      return divide(num1, num2);
+      break;
+  }
 }
 
 // display
 
-function showNumbers (value){
- screenValue.innerText=value;
- if(value===undefined)screenValue.innerText=" ";
+function showNumbers(value) {
+  screenValue.innerText = value;
+  if (value === undefined) screenValue.innerText = " ";
 }
 
-function showOperation(operator){
- screenOperation.innerText=operator;
+function showOperation(operator) {
+  screenOperation.innerText = operator;
 }
-
-
