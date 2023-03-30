bfb1d9fba7d758792ead34783a4f7426338334db
1slander <hectorgomendez@gmail.com>
2023-03-30 12:45:46 +0100

    add css

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ let operator;
 let active = false;
 let equalActive = false;
 
-
 //Events listeners
 
 btnDigits.forEach((button) => {
@@ -57,8 +56,6 @@ btnOperator.forEach((button) => {
   });
 });
 
-
-
 btnDot.addEventListener("click", () => {
   if (!active) {
     let newValue = value + ".";
@@ -90,8 +87,12 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  if (num2 === 0) return "ERROR";
-  return num1 / num2;
+  if (num2 === 0) {
+    alert("Error Can't divide by 0");
+    return 0;
+  } else {
+    return num1 / num2;
+  }
 }
 
 //Operate
@@ -100,7 +101,6 @@ function operate(num1, operator, num2) {
   let a = parseInt(num1);
   let b = parseInt(num2);
   let result = "";
-  
 
   switch (operator) {
     case "+":
@@ -117,16 +117,14 @@ function operate(num1, operator, num2) {
       result = divide(a, b);
       break;
   }
-  
-  
-  return Math.round(1000000*result)/1000000;
+
+  return Math.round(1000000 * result) / 1000000;
 }
 
 // Display
 
 function showNumbers(value) {
   screenValue.innerText = `${value}`;
-
 }
 
 function showOperation(operator) {
@@ -140,14 +138,14 @@ function addOperator() {
     showNumbers(total);
   }
   if (!num1 && !num2) {
-    console.log('1. !NUM1 & !NUM2')
+    console.log("1. !NUM1 & !NUM2");
     num1 = value;
     //console.log("Num1:", num1);
     value = "";
     //console.log("Value when num1 is defined:", value);
   } else if (num1 !== undefined && num2 === undefined) {
     //console.log("Value of num1 when no num2:", num1);
-    console.log('2. NUM1 & !NUM2')
+    console.log("2. NUM1 & !NUM2");
     num2 = value;
     //console.log("This Num2:", num2);
     total = operate(num1, operator, num2);
@@ -159,10 +157,10 @@ function addOperator() {
     //console.log("This Num2 Should be Undefined:", num2);
     value = "";
   } else if (num1 !== undefined && num2 !== undefined) {
-    console.log('3. NUM1 & NUM2')
+    console.log("3. NUM1 & NUM2");
     //console.log("THIS PART IS WORKING");
     total = operate(num1, operator, num2);
-   // console.log(`This total when num2 is defined: ${total}`);
+    // console.log(`This total when num2 is defined: ${total}`);
     showNumbers(total);
     num1 = total;
     num2 = value;
@@ -189,19 +187,18 @@ function clearScreen() {
 }
 
 function equal() {
-  if(!num1 && !num2){
+  if (!num1 && !num2) {
     return;
     //console.log(value)
   } else {
     equalActive = true;
     //console.log(num1)
-   // console.log(num2)
+    // console.log(num2)
     num2 = value;
     total = Number(operate(num1, operator, num2));
 
     showNumbers(total);
   }
-  
+
   //console.log('THis total:', total)
-  
 }
